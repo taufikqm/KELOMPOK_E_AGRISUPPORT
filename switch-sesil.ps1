08eb0d9e56a3cb30a2acdf5969413dfa5d6804cf
Taufik <taufikqm8@gmail.com>
2026-04-25 03:32:10 +0700

    chore: hapus switch scripts dari tracking git dan tambahkan ke .gitignore

--- switch-sesil.ps1
+++
@@ -1,4 +0,0 @@
-git config user.name "Daenisty"
-git config user.email "daenistycecillia@gmail.com"
-git remote set-url --push origin https://Daenisty@github.com/taufikqm/KELOMPOK_E_AGRISUPPORT.git
-Write-Host "Switched to Sesil (Daenisty)" -ForegroundColor Green
